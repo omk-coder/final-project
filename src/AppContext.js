38b6dd290da6b600365bc365ee17c18b7714f467
OMKAR <ramishteomkar@gmail.com>
2024-03-29 08:20:47 +0530

    name is changed from them login page

--- src/AppContext.js
+++ src/AppContext.js
@@ -10,7 +10,7 @@ fcl
   .put('flow.network', 'testnet')
   .put('accessNode.api', 'https://rest-testnet.onflow.org') //Api URL for the Flow Blockchain Access Node you want to be communicating with.
   .put('discovery.wallet', 'https://fcl-discovery.onflow.org/testnet/authn') // Points FCL at the Wallet or Wallet Discovery mechanism.
-  .put('app.detail.title', 'CryptoKnights')
+  .put('app.detail.title', 'Chain-Checkmate')
   .put('app.detail.icon', 'https://crypto-checkmate.vercel.app/imgs/logo.png')
   .put('0xChessKnights', '0xed4dad55d4060467')
   .put('0xChessOracleElo', '0xed4dad55d4060467');
